20c649319c8c6dd8a39ba01589f347cbbdd1696d
Di4ik3r <di4iker@gmail.com>
2020-07-15 17:26:36 +0300

    convert project files from spaces to tabs

--- components/App.js
+++ components/App.js
@@ -8,30 +8,30 @@
 
 import React, { useState } from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-  Button,
-  TextInput,
+	SafeAreaView,
+	StyleSheet,
+	ScrollView,
+	View,
+	Text,
+	StatusBar,
+	Button,
+	TextInput,
 } from 'react-native';
 
 import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
+	Header,
+	LearnMoreLinks,
+	Colors,
+	DebugInstructions,
+	ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
 import {
-  NativeRouter,
-  Route,
-  Link,
-  Redirect,
-  useHistory,
+	NativeRouter,
+	Route,
+	Link,
+	Redirect,
+	useHistory,
 } from 'react-router-native';
 
 import t from 'tcomb-form-native';
@@ -49,72 +49,72 @@ import reducer from '../redux/reducer';
 const store = createStore(reducer);
 
 const App = () => {
-  return (
-    <Provider store={store}>
-      <NativeRouter>
-        <View style={styles.body}>
-          <Route exact path="/" component={Index} />
-          <Route path="/app" component={AppIndex} />
-        </View>
-      </NativeRouter>
-    </Provider>
-  );
+	return (
+		<Provider store={store}>
+			<NativeRouter>
+				<View style={styles.body}>
+					<Route exact path="/" component={Index} />
+					<Route path="/app" component={AppIndex} />
+				</View>
+			</NativeRouter>
+		</Provider>
+	);
 };
 
 const AppIndex = () => {
-  return (
-    <View>
-      <Route path="/app/home" component={Home} />
-      <Route path="/app/add" component={Add} />
-      <TabBar />
-    </View>
-  );
+	return (
+		<View>
+			<Route path="/app/home" component={Home} />
+			<Route path="/app/add" component={Add} />
+			<TabBar />
+		</View>
+	);
 };
 
 const Index = () => {
-  return (
-    <>
-      <Login />
-    </>
-  );
+	return (
+		<>
+			<Login />
+		</>
+	);
 };
 
 const styles = StyleSheet.create({
-  body: {
-    width: '100%',
-    height: '100%',
-    backgroundColor: '#d1d1d1',
-  },
-  input: {
-    borderBottomWidth: 1,
-    borderBottomColor: 'grey',
-    margin: 5,
-    marginBottom: 15,
-    padding: 0,
-  },
-  button: {
-    marginTop: 10,
-    marginLeft: 20,
-    marginRight: 20,
-  },
-  text: {
-    width: '100%',
-    textAlign: 'center',
-    alignSelf: 'center',
-    textTransform: 'uppercase',
-    fontWeight: 'bold',
-    fontSize: 20,
-    textAlignVertical: 'center',
-    height: '100%',
-  },
-  appChild: {
-    height: '90%',
-  },
+	body: {
+		width: '100%',
+		height: '100%',
+		backgroundColor: '#d1d1d1',
+	},
+	input: {
+		borderBottomWidth: 1,
+		borderBottomColor: 'grey',
+		margin: 5,
+		marginBottom: 15,
+		padding: 0,
+	},
+	button: {
+		marginTop: 10,
+		marginLeft: 20,
+		marginRight: 20,
+	},
+	text: {
+		width: '100%',
+		textAlign: 'center',
+		alignSelf: 'center',
+		textTransform: 'uppercase',
+		fontWeight: 'bold',
+		fontSize: 20,
+		textAlignVertical: 'center',
+		height: '100%',
+	},
+	appChild: {
+		height: '90%',
+	},
 });
 
 function randomInteger(min, max) {
-  let rand = min + Math.random() * (max - min);
-  return Math.floor(rand);
+	let rand = min + Math.random() * (max - min);
+	return Math.floor(rand);
 }
 
 export { styles, randomInteger };
--- components/TabBar.js
+++ components/TabBar.js
@@ -7,88 +7,88 @@ import { styles as appStyles } from './App';
 import { logout as logoutAction } from '../redux/actions';
 
 const TabBar = ({ logout }) => {
-  return (
-    <View className="tab-bar" style={styles.tabBar}>
-      <TabBarItem position="left" text="home" linkTo="/app/home" />
-      <TabBarItem position="center" text="add" linkTo="/app/add" />
-      <TabBarItemLogout logout={logout} text={'Logout'} />
-    </View>
-  );
+	return (
+		<View className="tab-bar" style={styles.tabBar}>
+			<TabBarItem position="left" text="home" linkTo="/app/home" />
+			<TabBarItem position="center" text="add" linkTo="/app/add" />
+			<TabBarItemLogout logout={logout} text={'Logout'} />
+		</View>
+	);
 };
 
 const TabBarItem = ({ text, linkTo, position }) => {
-  let style = styles.tabBarItem;
-  switch (position) {
-    case 'left': {
-      style = {
-        ...style,
-        ...styles.tabBarLeftItem,
-      };
-      break;
-    }
-    case 'center': {
-      style = {
-        ...style,
-        ...styles.tabBarCenterItem,
-      };
-      break;
-    }
-  }
+	let style = styles.tabBarItem;
+	switch (position) {
+		case 'left': {
+			style = {
+				...style,
+				...styles.tabBarLeftItem,
+			};
+			break;
+		}
+		case 'center': {
+			style = {
+				...style,
+				...styles.tabBarCenterItem,
+			};
+			break;
+		}
+	}
 
-  return (
-    <Link to={linkTo} className="tab-bar-item" style={style}>
-      <Text style={appStyles.text}> {text} </Text>
-    </Link>
-  );
+	return (
+		<Link to={linkTo} className="tab-bar-item" style={style}>
+			<Text style={appStyles.text}> {text} </Text>
+		</Link>
+	);
 };
 
 const TabBarItemLogout = ({ text, logout }) => {
-  return (
-    <Link
-      to="/"
-      className="tab-bar-item"
-      style={{
-        ...styles.tabBarItem,
-        ...styles.tabBarRightItem,
-      }}
-      onPress={() => {
-        logout();
-      }}
-    >
-      <Text style={appStyles.text}> {text} </Text>
-    </Link>
-  );
+	return (
+		<Link
+			to="/"
+			className="tab-bar-item"
+			style={{
+				...styles.tabBarItem,
+				...styles.tabBarRightItem,
+			}}
+			onPress={() => {
+				logout();
+			}}
+		>
+			<Text style={appStyles.text}> {text} </Text>
+		</Link>
+	);
 };
 
 const styles = StyleSheet.create({
-  tabBar: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'stretch',
-    height: '10%',
-    backgroundColor: 'transparent',
-  },
-  tabBarItem: {
-    flex: 1,
-    borderTopWidth: 2,
-    borderTopColor: '#3a3a3a',
-    backgroundColor: '#a3a3a3',
-  },
-  tabBarLeftItem: {
-    borderTopLeftRadius: 25,
-    borderLeftWidth: 2,
-  },
-  tabBarCenterItem: {},
-  tabBarRightItem: {
-    borderTopRightRadius: 25,
-    borderRightWidth: 2,
-  },
+	tabBar: {
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'stretch',
+		height: '10%',
+		backgroundColor: 'transparent',
+	},
+	tabBarItem: {
+		flex: 1,
+		borderTopWidth: 2,
+		borderTopColor: '#3a3a3a',
+		backgroundColor: '#a3a3a3',
+	},
+	tabBarLeftItem: {
+		borderTopLeftRadius: 25,
+		borderLeftWidth: 2,
+	},
+	tabBarCenterItem: {},
+	tabBarRightItem: {
+		borderTopRightRadius: 25,
+		borderRightWidth: 2,
+	},
 });
 
 const mapStateToProps = (state) => ({});
 const mapDispatchToProps = (dispatch) => ({
-  logout: () => dispatch(logoutAction()),
+	logout: () => dispatch(logoutAction()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TabBar);
