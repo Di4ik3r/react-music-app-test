4d0b78b6b8eff70fa8bcb644b3b8fbe4eb1ae748
Di4ik3r <di4iker@gmail.com>
2020-07-15 17:15:54 +0300

    convert from spaces to tab

--- components/Track.js
+++ components/Track.js
@@ -5,65 +5,65 @@ import { connect } from 'react-redux';
 import { removeTrack as removeTrackAction } from '../redux/actions';
 
 const Track = ({ track, removeTrack }) => {
-  const handlePress = () => {
-    removeTrack(track.id);
-  };
+	const handlePress = () => {
+		removeTrack(track.id);
+	};
 
-  return (
-    <TouchableOpacity
-      onPress={() => {
-        handlePress();
-      }}
-      style={styles.track}
-    >
-      <Text
-        style={{
-          ...styles.trackText,
-          ...styles.trackTextBand,
-        }}
-      >
-        {track.band}
-      </Text>
-      <Text
-        style={{
-          ...styles.trackText,
-          ...styles.trackTextHeader,
-        }}
-      >
-        Track({track.id}): <Text> {track.track} </Text>
-      </Text>
-    </TouchableOpacity>
-  );
+	return (
+		<TouchableOpacity
+			onPress={() => {
+				handlePress();
+			}}
+			style={styles.track}
+		>
+			<Text
+				style={{
+					...styles.trackText,
+					...styles.trackTextBand,
+				}}
+			>
+				{track.band}
+			</Text>
+			<Text
+				style={{
+					...styles.trackText,
+					...styles.trackTextHeader,
+				}}
+			>
+				Track({track.id}): <Text> {track.track} </Text>
+			</Text>
+		</TouchableOpacity>
+	);
 };
 
 const styles = StyleSheet.create({
-  track: {
-    margin: 5,
-    padding: 5,
-    borderColor: 'black',
-    borderWidth: 1,
-    borderRadius: 10,
-    backgroundColor: '#b3b3b3',
-  },
-  trackText: {
-    textAlign: 'center',
-  },
-  trackTextBand: {
-    fontWeight: 'bold',
-    fontSize: 16,
-  },
-  trackTextHeader: {
-    fontWeight: '800',
-    fontSize: 14,
-  },
+	track: {
+		margin: 5,
+		padding: 5,
+		borderColor: 'black',
+		borderWidth: 1,
+		borderRadius: 10,
+		backgroundColor: '#b3b3b3',
+	},
+	trackText: {
+		textAlign: 'center',
+	},
+	trackTextBand: {
+		fontWeight: 'bold',
+		fontSize: 16,
+	},
+	trackTextHeader: {
+		fontWeight: '800',
+		fontSize: 14,
+	},
 });
 
 const mapStateToProps = (state) => ({});
 
 const mapDispatchToProps = (dispatch) => ({
-  removeTrack: (id) => {
-    dispatch(removeTrackAction(id));
-  },
+	removeTrack: (id) => {
+		dispatch(removeTrackAction(id));
+	},
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Track);
